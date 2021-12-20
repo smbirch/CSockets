853988a6435992e260046a7b88f8b1228ddb69cd
smbirch <spencermbirch@gmail.com>
2021-12-20 14:18:04 -0800

    adding binder, listener, and accepting incoming connections

--- server.c
+++ server.c
@@ -4,5 +4,44 @@
 
 int main(int argc, char *argv[])
 {
+    int socket_desc, new_socket, c;
+    struct sockaddr_in server, client;
+
+    //Create socket
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket!\n");
+    }
+
+    //Prepare sockaddr_in struct
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(8888);
+
+    //Bind
+    if (bind(socket_desc,(struct sockaddr *) &server, sizeof(server)) < 0)    
+    {
+        puts("Bind failed!\n");
+    }
+    puts("Bind complete\n");
+
+    //Listen
+    listen(socket_desc, 3);
+
+    // Accept incoming connections
+    puts("Waiting for incoming connections...\n");
+    c = sizeof(struct sockaddr_in);
+    new_socket = accept(socket_desc, (struct sockaddr *) &client, (socklen_t*)&c);
+
+    if (new_socket < 0)
+    {
+        perror("accept failed\n");
+    }
+
+    puts ("Connection accepted\n");
+
+    return 0;
+
     
 }
